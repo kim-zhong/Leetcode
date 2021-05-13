273f8698d04e76e2299539d2a762a8c8465eb731
kim-zhong <kimzhong2017@gmail.com>
2021-05-13 23:01:21 +0800

    start from 13/05/2021

---
+++ linkedlist/hasCycle.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+    
+public:
+    bool hasCycle(ListNode *head) {
+        if (head==NULL) return false;
+        ListNode *small = head;  
+        ListNode *large = head->next;
+        
+        while(small!=large){
+            if (large ==NULL || large->next==NULL){
+                return false;
+            }
+            if (small == NULL){
+                return false;
+            
+            }
+            small = small->next;
+            
+            large = large->next->next;
+        
+        }
+        return true;
+        
+    }
+};
